c70e8b2aeb352f1c1df1b61f14ed4be806dd3816
AndrewGudi <jamesbanner1225@gmail.com>
2021-10-13 12:41:21 +0300

    1 урок
    адаптировал сайт

--- lesson1/src/js/script.js
+++ lesson1/src/js/script.js
@@ -1,16 +1,46 @@
+"use strict"
+
+let visible = false;
+let visibility = false;
+var i, tablinks;
+
+
+
 function openItem(evt, item) {
-	var i, maincontent, tablinks;
-
-	maincontent = document.getElementsByClassName("main-content");
-	for (i = 0; i < maincontent.length; i++) {
-		maincontent[i].style.display = "none";
-	}
-
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 
-	document.getElementById(item).style.display = "block";
-	evt.currentTarget.className += " active";
+	if (visible) {
+		document.getElementById(item).style.display = 'none';
+		visible = false;
+	} else {
+		document.getElementById(item).style.display = 'block';
+		visible = true;
+		evt.currentTarget.className += " active";
+	}
+
+
+}
+
+
+
+
+function openMenu(evt, item) {
+	tablinks = document.getElementsByClassName("item-header__button");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	if (visibility) {
+		document.getElementById(item).style.display = 'none';
+		visibility = false;
+	} else {
+		document.getElementById(item).style.display = 'block';
+		visibility = true;
+		evt.currentTarget.className += " active";
+	}
+
+
 }
